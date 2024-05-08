b89dd10b2900c64b1ea5e9103fbbc68f62f87235
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-05-08 22:33:02 +0530

    Maanas-Added ejs files and made routes for faculty

--- src/routes/faculty.ts
+++ src/routes/faculty.ts
@@ -1,16 +1,16 @@
 import express from "express";
-import {
-  getFaculty,
-  getFacultyById,
-  updateFaculty,
-  deleteFaculty,
-  createFaculty,
-} from "../controllers/faculty";
+import {getFaculty, getFacultyById, updateFaculty, renderDeleteForm, deleteFaculty, deleteAll, createFaculty, renderCreateForm, renderUpdateForm, viewFaculty} from "../controllers/faculty";
 const router = express.Router();
 
-router.get("/", getFaculty);
-router.get("/:id", getFacultyById);
+router.get("/create", renderCreateForm);
+router.get("/:id/update", renderUpdateForm);
+router.get("/:id/delete", renderDeleteForm);
+router.get("/view", viewFaculty);
+
 router.post("/", createFaculty);
 router.put("/:id", updateFaculty);
+router.get("/deleteAll", deleteAll);
 router.delete("/:id", deleteFaculty);
+router.get("/:id", getFacultyById);
+router.get("/", getFaculty);
 export default router;
