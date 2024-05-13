6fd8cc6571acd8cd7f4adfa9e3a4f34e9f6c5eca
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-05-13 14:58:09 +0530

    completed faculty routes

--- src/controllers/faculty.ts
+++ src/controllers/faculty.ts
@@ -25,7 +25,9 @@ export const getFaculty = async (req: Request, res: Response) => {
 export const viewFaculty = async (req: Request, res: Response) => {
     try {
         const facultyList = await FacultyModel.find();
-        if (!facultyList) return res.status(404).json({message: "No records found !"});
+        if (!facultyList) {
+            return res.status(404).json({message: "No records found !"});
+        }
         res.status(200).render("Faculty/facultyList", {list: facultyList});
     } catch (err) {
         console.log(err);
@@ -33,56 +35,50 @@ export const viewFaculty = async (req: Request, res: Response) => {
     }
 };
 
-export const getFacultyById = async (req: Request, res: Response) => {
+export const getFacultyById = async (id: String) => {
     try {
-        const {id} = req.params;
-        console.log(id);
-        if (!id || id === ":id") return res.status(400).json({message: "Id is required"});
         const faculty = await FacultyModel.findById(id);
-        if (!faculty) return res.status(404).json({message: "Faculty not found"});
-        res.status(200).render("Faculty/facultyDetails", {faculty});
+        if (!faculty) {
+            return {status: 404, message: "Faculty not found"};
+        } else {
+            return {status: 200, faculty};
+        }
     } catch (err) {
         console.log(err);
-        res.status(500).send("Internal Server Error");
+        return {status: 500, message: "Internal Server Error"};
     }
 };
 
-export const renderUpdateForm = async (req: Request, res: Response) => {
-    const id = req.params.id;
-    const faculty = await FacultyModel.findById(id);
-    if (!faculty) {
-        return res.status(404).json({message: "Faculty not found"});
-    }
-
-    res.render("Faculty/updateForm", {faculty});
-};
-
-export const updateFaculty = async (req: Request, res: Response) => {
+export const findFaculty = async (id: String) => {
     try {
-        const {id} = req.params;
-        const {name, experience, photoUrl}: Faculty = req.body;
-        if (!name || !experience || !photoUrl) return res.status(400).json({message: "Name, experience, and photoUrl are required"});
         const faculty = await FacultyModel.findById(id);
-        if (!faculty) return res.status(404).json({message: "Faculty not found"});
-        faculty.name = name;
-        faculty.experience = experience;
-        faculty.photoUrl = photoUrl;
+        if (!faculty) {
+            return {status: 404, message: "Faculty not found"};
+        } else {
+            return {status: 200, faculty};
+        }
+    } catch (err) {
+        console.log(err);
+        return {status: 500, message: "Internal Server Error"};
+    }
+};
+
+export const updateFaculty = async (id: String, updatedName?: String, updatedPhotoUrl?: String, updatedExperience?: string) => {
+    try {
+        const faculty = await FacultyModel.findById(id);
+        if (!faculty) {
+            return {status: 404, message: "Faculty not found"};
+        }
+        faculty.name = updatedName ? String(updatedName) : "";
+        faculty.experience = updatedExperience ? String(updatedExperience) : "";
+        faculty.photoUrl = updatedPhotoUrl ? String(updatedPhotoUrl) : null;
+
         await faculty.save();
-        res.status(200).json({message: "Faculty updated successfully", faculty});
+        return {status: 200, faculty};
     } catch (err) {
         console.log(err);
-        res.status(500).send("Internal Server Error");
+        return {status: 500, message: "Internal Server Error"};
     }
-};
-
-export const renderDeleteForm = async (req: Request, res: Response) => {
-    const id = req.params.id;
-    const faculty = await FacultyModel.findById(id);
-    if (!faculty) {
-        return res.status(404).json({message: "Faculty not found"});
-    }
-
-    res.render("Faculty/deleteForm", {faculty});
 };
 
 export const deleteAll = async (req: Request, res: Response) => {
@@ -95,37 +91,30 @@ export const deleteAll = async (req: Request, res: Response) => {
     }
 };
 
-export const deleteFaculty = async (req: Request, res: Response) => {
+export const deleteFaculty = async (id: String) => {
     try {
-        const {id} = req.params;
-        if (!id) return res.status(400).json({message: "Id is required"});
         const faculty = await FacultyModel.findById(id);
-        if (!faculty) return res.status(404).json({message: "Faculty not found"});
+        if (!faculty) {
+            return {status: 404, message: "Faculty not found"};
+        }
         await faculty.deleteOne();
-        res.status(200).json({message: "Faculty deleted successfully"});
+        return {status: 200, faculty};
     } catch (err) {
         console.log(err);
-        res.status(500).send("Internal Server Error");
+        return {status: 500, message: "Internal Server Error"};
     }
 };
 
-export const renderCreateForm = async (req: Request, res: Response) => {
-    res.render("Faculty/facultyForm");
-};
-
-export const createFaculty = async (req: Request, res: Response) => {
+export const createFaculty = async (experience: Number, name?: String, photoUrl?: String) => {
     try {
-        const {name, experience, photoUrl}: Faculty = req.body;
-        if (!name || !experience || !photoUrl) {
-            res.status(400).json({message: "Name, experience, and photoUrl are required"});
-            return;
-        }
         const faculty = new FacultyModel({name, experience, photoUrl});
         await faculty.save();
+
         console.log(faculty);
-        res.status(201).json({message: "Faculty created successfully", faculty});
+
+        return {status: 201, faculty};
     } catch (err) {
         console.log(err);
-        res.status(500).send("Internal Server Error");
+        return {status: 500, message: "Internal Server Error"};
     }
 };
