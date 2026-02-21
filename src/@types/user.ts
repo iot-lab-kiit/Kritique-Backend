ed47288c4bb16e501f1d023ec8c1a3c0fbb1d79f
JagTheFriend <JagTheFriend12@gmail.com>
2026-02-21 21:48:21 +0530

    Renamed field

--- src/@types/user.ts
+++ src/@types/user.ts
@@ -6,7 +6,7 @@ export interface User {
   photoUrl?: string | null;
   role?: "admin" | "user";
   status?: boolean;
-  waitList?: string[];
+  bookmark: string[];
 }
 
 export interface UserQuery {
